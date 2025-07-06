97a1e65eb98a9741f2dba271ec25263b0e779f15
imranme <tusharimran914076@gmail.com>
2025-07-06 09:32:31 +0600

    database added

--- bd_bank/settings.py
+++ bd_bank/settings.py
@@ -96,23 +96,23 @@ WSGI_APPLICATION = 'bd_bank.wsgi.application'
 # }
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         # Feel free to alter this value to suit your needs.
-#         default='postgresql://bdbank_user:e8jHaJ73QjaOgGyx68vkDUVTqnHX7wdz@dpg-ctdoikaj1k6c73ds4qf0-a.oregon-postgres.render.com/bdbank',
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': env("DB_NAME"),
+#         'USER': env("DB_USER"),
+#         'PASSWORD': env("DB_PASSWORD"),
+#         'HOST': env("DB_HOST"),
+#         'PORT': env("DB_PORT"),
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://bdbank_user:e8jHaJ73QjaOgGyx68vkDUVTqnHX7wdz@dpg-ctdoikaj1k6c73ds4qf0-a.oregon-postgres.render.com/bdbank',
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
